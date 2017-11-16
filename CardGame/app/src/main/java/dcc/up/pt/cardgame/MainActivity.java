51382f46fd7f10e75726291cf81b8c383344c7cf
Satyan JACQUENS <satyan.jacquens@grenoble-inp.org>
2017-11-16 23:31:00 +0000

    Cleanup
    - Change the build method: Now uses CMake ( to install in Android Studio)
    - Make fields private in MainActivity
    - Remove useless stuff
    - Enable the camera view if the permission is already granted

---
+++ CardGame/app/src/main/java/dcc/up/pt/cardgame/MainActivity.java
@@ -0,0 +1,121 @@
+package dcc.up.pt.cardgame;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    private static final String TAG = "MainActivity";
+    public static final int CAMERA_PERMISSION_REQUEST = 100;
+    private static final String[] PERMISSIONS = new String[]{Manifest.permission.CAMERA};
+
+    private  JavaCameraView mCameraView;
+    private Mat mRgba, imgGray, imgCanny;
+    private BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this){
+        @Override
+        public void onManagerConnected(int status){
+            switch(status){
+                case BaseLoaderCallback.SUCCESS:{
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.CAMERA)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                PERMISSIONS,
+                                CAMERA_PERMISSION_REQUEST);
+                    } else {
+                        mCameraView.enableView();
+                    }
+                    break;
+                }
+                default:{
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCameraView = findViewById(R.id.camera_view);
+        mCameraView.setVisibility(SurfaceView.VISIBLE);
+        mCameraView.setCvCameraViewListener(this);
+
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV not loaded!\n");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_3_0,this,mLoaderCallBack);
+
+        } else{
+            Log.d(TAG, "OpenCV loaded!\n");
+            mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mCameraView.disableView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCameraView.enableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height){
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        imgGray = new Mat(height, width, CvType.CV_8UC1);
+        imgCanny = new Mat(height, width, CvType.CV_8UC1);
+    }
+
+    @Override
+    public void onCameraViewStopped(){
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame){
+        mRgba = inputFrame.rgba();
+        //Imgproc.cvtColor(mRgba, imgGray, Imgproc.COLOR_RGB2GRAY);
+        return mRgba;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_PERMISSION_REQUEST: {
+                Log.i("Camera", "G : " + grantResults[0]);
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mCameraView.enableView();
+                } else {
+                    finishAndRemoveTask();
+                }
+            }
+
+        }
+    }
+
+}
